37b2de918abadedc9dfebef8ef20a25784540aea
Alija Bobija <bobija.alija@gmail.com>
2019-06-03 12:04:51 +0200

    Lua code

---
+++ esp32/init.lua
@@ -0,0 +1,15 @@
+local rc522 = require('rc522_32')().init()
+
+local timer = tmr.create()
+
+timer:register(250, tmr.ALARM_AUTO, function()
+
+    local tag = rc522.tag()
+
+    if tag ~= nil then
+        print( rc522.tag_hex(tag) )
+    end
+    
+end)
+
+timer:start()
